947200dfc525695cea022964db954610065ea804
Yuriy <y.gavrinskiy@mail.ru>
2020-05-23 00:51:19 +0300

    день 10 верстка карточки бронирования номера, регистрации аккаунта

---
+++ src/components/reg-form/reg-form.js
@@ -0,0 +1,5 @@
+import './reg-form.scss'
+
+export default function regForm () {
+    
+}
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./scss/style.scss";
 
 import searchForm from "./components/search-form/search-form"
 import cards from "./components/cards/cards"
+import regForm from "./components/reg-form/reg-form"
