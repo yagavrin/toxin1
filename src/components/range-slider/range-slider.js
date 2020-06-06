e809975339427de0552c3135a3d1b35099df250b
Yuriy <y.gavrinskiy@mail.ru>
2020-06-06 03:03:54 +0300

    день 23. доработан  range slider

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -3,15 +3,13 @@ import './range-slider.scss'
 export default function rangeSlider () {
   let startPoint = document.querySelector('.range-slider__point_start');
   let finishPoint = document.querySelector('.range-slider__point_finish');
-  let slider = document.querySelector('.range-slider__block');
-  let coloredBlock = document.querySelector('.range-slider__colored-block');
+  let slider = document.querySelector('.range-slider__scale');
+  let coloredBlock = document.querySelector('.range-slider__scale_colored');
   let bottom = document.querySelector('.range-slider__bottom');
   let top = document.querySelector('.range-slider__top');
-  const bottomText = +bottom.textContent;
-  const topText = +top.textContent;
+  const upperLimit = 16000;
   let newLeft;
-
-
+  
   startPoint.onmousedown = function(event) {
     event.preventDefault();
     let shiftX = event.clientX - startPoint.getBoundingClientRect().left;
@@ -24,23 +22,17 @@ export default function rangeSlider () {
       newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
 
       if (newLeft < -startPoint.offsetWidth/2) {
-        newLeft = 0 - startPoint.offsetWidth/2;
+        newLeft = -startPoint.offsetWidth/2;
       }
 
-      if (newLeft + startPoint.offsetWidth > finishPoint.getBoundingClientRect().left- slider.getBoundingClientRect().left) {
+      if (newLeft + startPoint.offsetWidth > finishPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left) {
         newLeft = finishPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left - startPoint.offsetWidth
       }
 
-      let rightEdge = slider.offsetWidth - startPoint.offsetWidth/2;
-      if (newLeft > rightEdge) {
-        newLeft = rightEdge;
-      }
-
       startPoint.style.left = newLeft + 'px';
-      coloredBlock.style.left = newLeft + startPoint.offsetWidth + 'px';
-      let coloredBlockWidth = finishPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left - newLeft
-      coloredBlock.style.width = coloredBlockWidth + 'px';
-      bottom.textContent= roundNum(Math.round((startPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left)/ 266 * topText)) + ' - ';
+      coloredBlock.style.left = newLeft + startPoint.offsetWidth/2 + 'px';
+      coloredBlock.style.width = finishPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left - newLeft + 'px';
+      bottom.textContent= roundNum(Math.round((startPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left + startPoint.offsetWidth /2 ) / (slider.offsetWidth - startPoint.offsetWidth) * upperLimit)) + '₽ - ';
     }
 
     function onMouseUp() {
@@ -52,7 +44,7 @@ export default function rangeSlider () {
   
   finishPoint.onmousedown = function(event) {
     event.preventDefault();
-    let shiftX = event.clientX - finishPoint.getBoundingClientRect().left + startPoint.offsetWidth;
+    let shiftX = event.clientX - finishPoint.getBoundingClientRect().left;
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
@@ -60,23 +52,19 @@ export default function rangeSlider () {
     function onMouseMove(event) {
       newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
 
-      if (newLeft < -startPoint.offsetWidth/2) {
-        newLeft = 0 - startPoint.offsetWidth/2;
-      }
-
       if (newLeft < startPoint.getBoundingClientRect().right - slider.getBoundingClientRect().left ) {
-        newLeft = startPoint.getBoundingClientRect().right - slider.getBoundingClientRect().left - startPoint.offsetWidth
+        newLeft = startPoint.getBoundingClientRect().right - slider.getBoundingClientRect().left
       }
 
-      let rightEdge = slider.offsetWidth - startPoint.offsetWidth/2 - startPoint.offsetWidth;
+      let rightEdge = slider.offsetWidth - startPoint.offsetWidth/2;
       if (newLeft > rightEdge) {
         newLeft = rightEdge;
       }
 
       finishPoint.style.left = newLeft + 'px';
-      let coloredBlockWidth = newLeft + startPoint.offsetWidth - (startPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left);
-      coloredBlock.style.width = coloredBlockWidth + 'px';
-      top.textContent= roundNum(Math.round((finishPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left)/ 266 * topText));
+
+      coloredBlock.style.width = newLeft + startPoint.offsetWidth - (startPoint.getBoundingClientRect().right - slider.getBoundingClientRect().left) + 'px';
+      top.textContent = roundNum(Math.round((finishPoint.getBoundingClientRect().left - slider.getBoundingClientRect().left - startPoint.offsetWidth / 2) / (slider.offsetWidth - startPoint.offsetWidth) * upperLimit)) + "₽";
     }
 
     function onMouseUp() {
@@ -94,17 +82,18 @@ export default function rangeSlider () {
   };
 
   function roundNum (num) {
-
-    let divisionRemainder = num % 100;
+    let divider = 100
+    let divisionRemainder = num % divider;
   
     if (divisionRemainder === 0) {
-      return num;
-    } else if (divisionRemainder > 50) {
-      return num - divisionRemainder + 100
+      return num.toLocaleString('ru');
+    } else if (divisionRemainder > divider/2) {
+      return (num - divisionRemainder + divider).toLocaleString('ru');
     } else {
-      return num - divisionRemainder
+      return (num - divisionRemainder).toLocaleString('ru');
     }
   }
 
 }
+
 
