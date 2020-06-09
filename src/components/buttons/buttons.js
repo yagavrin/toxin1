12f452fe13d614c1ed359920f85d71f4b58f8661
Yuriy <y.gavrinskiy@mail.ru>
2020-06-09 14:13:35 +0300

    день 28. доработна кнопка с градиентной заливкой

--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -9,8 +9,8 @@ export default function buttons () {
     if (btn) {
 
       if (btn.classList.contains('btn_border')) {
-        btn.style.background= 'rgba(188, 156, 255, 0.75)';
-        setTimeout( () => btn.style.background = 'linear-gradient(180deg, #BC9CFF 0%, #8BA4F9 100%)', 500);
+        btn.classList.add('btn_border_toggled');
+        setTimeout( () => btn.classList.remove('btn_border_toggled'), 500);
         return;
       }
 
@@ -18,14 +18,14 @@ export default function buttons () {
       setTimeout( () => btn.style.opacity = '', 500);
 
     };
-   
-  })
 
-  document.addEventListener('click', function(event) {
     let noBorderBtn = event.target.closest('.no-border-btn');
-    if (!noBorderBtn) return;
-    noBorderBtn.style.color = 'rgba(31, 32, 65, 0.5)';
+
+    if (noBorderBtn) {
+      noBorderBtn.style.color = 'rgba(31, 32, 65, 0.5)';
     setTimeout( () => noBorderBtn.style.color = '', 500);
+    }
+    
   })
   
 }
--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,12 +1,12 @@
 import './input.scss'
 
 export default function input() {
-    function loadScript(src) {
-        let script = document.createElement('script');
-        script.src = src;
-        script.async = false;
-        document.body.append(script);
-      }
-      loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js");
-      loadScript('/src/components/calendar/jquery.maskedinput.js');
+  function loadScript(src) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+  }
+  loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js");
+  loadScript('/src/components/calendar/jquery.maskedinput.js');
 }
