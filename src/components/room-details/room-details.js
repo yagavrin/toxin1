521863dbc0fc157e310f4279af3998328a6c27b3
Yuriy <y.gavrinskiy@mail.ru>
2020-06-25 01:41:19 +0300

    день 43. добавлен скрипт для полноэкранного просмотра фотографий на странице сведений о номере

--- src/components/room-details/room-details.js
+++ src/components/room-details/room-details.js
@@ -13,4 +13,29 @@ export default function roomDetails () {
       bookingBtn.firstElementChild.textContent = 'Забронировать номер';
     }
   }
+  
+  let background = document.querySelector('.room-details__background');
+  let closeBtn = document.querySelector('.room-details__background-close');
+  let activeItem = document.querySelector('.room-details__gallery-item_active');
+
+  document.addEventListener('click', function(event) {
+    let item = event.target.closest('.room-details__gallery-item');
+    if (item) {
+      background.classList.toggle('room-details__background_active');
+      activeItem.src = item.src;
+    }
+    if (event.target == background) {
+      background.classList.toggle('room-details__background_active');
+    }
+  })
+
+  activeItem.onmouseover = function() {
+    closeBtn.classList.toggle('room-details__background-close_active')
+  }
+
+  activeItem.onmouseout = function() {
+    console.log('переход');
+    closeBtn.classList.toggle('room-details__background-close_active')
+  }
+
 }
