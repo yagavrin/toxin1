b028cfdaad98ca4d760f8af325a4759acebca6fb
Yuriy <y.gavrinskiy@mail.ru>
2020-06-30 17:24:13 +0300

    день 49. добавлены настройки babel и оптимизация изображений

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    main: ['@babel/polyfill', './src/index.js'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: '[hash].[name].bundle.js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  devServer: {
+    port: 5600,
+    hot: true
   },
   module: {
     rules: [
@@ -14,7 +27,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
         }
       },
       {
@@ -23,46 +36,62 @@ module.exports = {
         options: {
             pretty: true
             }
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: './img/'
-              }
-            }
-          ]
-        },
-        {
-          test: /\.(ttf|woff|woff2|eot)$/,
-          use: [
-            {
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
             loader: 'file-loader',
             options: {
-                name: '[name].[ext]',
-                outputPath: './fonts/',
+              name: '[name].[ext]',
+              outputPath: './img/'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 70
+              }
             }
           }
         ]
-        },
-        {
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract(
-              {
-                fallback: 'style-loader',
-                use: ['css-loader', 'sass-loader']
-              })
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './fonts/',
             }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.pug',
-        }),
+      template: './src/index.pug',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ]
+    }),
+    // new CleanWebpackPlugin(),
     new ExtractTextPlugin({filename: 'style.css'}),
-    
   ]
 }
